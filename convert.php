4484bde4715f41ab33d379ca80b237aa7c0e1bff
Jason <jaemate21@yahoo.com>
2024-02-21 15:32:47 +0200

    enhancement of the application in general addition of javascript functionality

--- convert.php
+++ convert.php
@@ -1,50 +1,52 @@
 <?php
 
-$allowed_extentions = array('mp4') ;                 //an array of allowed extensions of the uploaded files
-$max_file_size = 10000000;                           //this is the maximum allowable size of the uploaded mp4 file, 1000000 = 10MB
+$file_name = $_FILES['upload']['name'];
+$file_size = $_FILES['upload']['size'];
+$file_tmp = $_FILES['upload']['tmp_name'];
 
-if(isset($_POST['submit']))                          //check if the submit button has been clicked(ie, has the form been submitted?)
+$allowed_extentions = array('mp4') ;
+$max_file_size = 10000000; 
+
+if(!empty($_FILES['upload']['name']))  
 {
-    if(!empty($_FILES['upload']['name']))            //check to see if the file actually uploaded
-    {
-        $file_name = $_FILES['upload']['name'];
-        $file_size = $_FILES['upload']['size'];
-        $file_tmp = $_FILES['upload']['tmp_name'];
-
-        // Get file extension
-        $file_extention = explode('.', $file_name);
-        $file_extention = strtolower(end($file_extention)); //Get the last entry of the array
+    // Get file extension
+    $file_extention = explode('.', $file_name);
+    $file_extention = strtolower(end($file_extention));
         
-        if(in_array($file_extention, $allowed_extentions))  // Validate file type/extension
+    if(in_array($file_extention, $allowed_extentions)) 
+    {
+        if($file_size <= $max_file_size)
         {
-            if($file_size <= $max_file_size) // 10000000 bytes = 10MB
-            {
-               $random_dir_name = uniqid();  //generate a random number that will be used to name a directory
-               mkdir("uploads/${random_dir_name}"); 
-               $target_dir = ("uploads/${random_dir_name}");
-               $target_file = ("$target_dir/${file_name}");
+            $random_dir_name = uniqid();
+            mkdir("uploads/${random_dir_name}", 0777); 
+            $target_dir = ("uploads/${random_dir_name}");
+            $target_file = ("$target_dir/${file_name}");
 
-               move_uploaded_file($file_tmp, $target_file);  //move the uploaded file from the temporary directory to the operation directory
-      
-               $output = shell_exec("cd $target_dir; ../../convert.sh; ls *.mp3 ");
-               $url = "$target_dir/${output}";
-               echo '<h2 style="color: green;">Success!! Your file is ready for download!</h2>';
-               echo "<a href='$url' Download=''>Download it!</a>";
-            }
-            else
-            {
-               echo '<p style="color: red;">File too large!</p>';
-            }
+            move_uploaded_file($file_tmp, $target_file);
+     
+            $output = shell_exec("cd $target_dir; ../../convert.sh; ls *.mp3 ");
+            $url = "$target_dir/${output}";
+
+            echo $url;
         }
         else
         {
-            echo '<p style="color: red;">Ooops,the the wrong file extension was submited</p>'; 
+        	//1 = File too large
+            echo '3';
+            exit();
         }
-    } 
+    }
     else
     {
-        echo '<p style="color: red;">Please choose a file</p>';
+    	//2 = wrong file extension
+        echo '2'; 
+        exit();
     }
+} 
+else
+{
+	//3 = No file submitted
+    echo '1';
 }
 
 ?>
